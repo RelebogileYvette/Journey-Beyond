fc9be34c1917fa317ab3ff76aab6d258bf8a5c46
rsebokolodi023 <rsebokolodi023@student.wethinkcode.co.za>
2024-10-13 21:32:04 +0200

    Just starte

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as sl
+
+sl.set_page_config(page_title="Jouney Beyond Web", page_icon=":tada:",layout="wide")
+
+sl.subheader("Hi, I am Relebogile :wave:")
+sl.title(" An inspiring Software Quality Assurance")
+sl.write("I am a final-year Software Engineering student at WeThinkCode and an aspiring Software Quality Assurance professional with a strong focus on web development and quality assurance. I have hands-on experience with tools and technologies such as GitLab, Git, Docker, Python, and Java, and am dedicated to building reliable and efficient software solutions. My collaborative skills have been demonstrated through participation in multiple hackathons and at campus i had been working in teams.")
+sl.write("[Learn More >](https://www.linkedin.com/in/relebogile-sebokolodi/)")
